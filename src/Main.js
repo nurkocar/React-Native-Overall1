a8f19e7c5917e0937ff76715d5477db7e78490a8
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-17 16:16:52 +0000

    completed first example

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity,StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, Alert } from 'react-native';
 
 import Button from './components/Button'
 
 const App = () => {
+
+    function sayHello() {
+        alert("Hello there!")
+    }
+
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1}}>
             <View>
-                <Text style = {{fontSize:50}}>Hello World</Text>
+                <Text style={{fontSize: 50}}>Hello World</Text>
             </View>
 
-            <Button />
+            <Button
+                myOnPress={() => sayHello()}
+                //users={[1, 2, 3, 4, 5]}
+                //count={189}
+                //number = {3}
+                title='Press Me'
+                color='purple'
+            />
+
+            <Button
+                myOnPress={() => {
+                    Alert.alert('Clarusway', 'FullStack 4ever!')
+                }}
+                //users={[1, 2, 3, 4, 5]}
+                //count={189}
+                title='Press Me and Say Hi'
+            />
+
 
         </SafeAreaView>
     )
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = () => {
+const Button = (props) => {
+
+    console.log(props);
+
+    let selectedColor = props.color;
+
+    if (props.color == undefined)
+        selectedColor = '#a5d6a7'
+
     return (
-        <TouchableOpacity style={styles.buttonContainer}>
-            <Text>Do Not Press Me!</Text>
+        <TouchableOpacity
+            style={
+                [styles.buttonContainer,{ backgroundColor: selectedColor }]
+            }
+            onPress={props.myOnPress}
+        >
+            <Text>{props.title}</Text>
         </TouchableOpacity>
     )
 }
@@ -12,7 +25,7 @@ const Button = () => {
 export default Button;
 
 const styles = StyleSheet.create({
-    buttonContainer:{
+    buttonContainer: {
         backgroundColor: '#a5d6a7',
         padding: 15,
         margin: 10,
@@ -21,3 +34,5 @@ const styles = StyleSheet.create({
     }
 })
 
+
+// backgroundColor: props.color === undefined ? '#a5d6a7' : props.color  Boyle yazarsak yukaridaki degisken tanimlamasina ve if condition a gerek kalmaz.
